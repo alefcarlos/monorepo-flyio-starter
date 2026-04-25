37242ebe22c3f7bf248c3262ac78199c91c099a1
alefcarlos <alef.carlos@gmail.com>
2026-04-24 22:27:54 -0300

    fix: remove Weather

--- src/Flyio.Demo.ApiService/Program.cs
+++ src/Flyio.Demo.ApiService/Program.cs
@@ -52,22 +52,6 @@ app.UseProblemDetailsWithDefaults();
 
 app.MapDefaultWebApiEndpoints();
 
-string[] summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.MapGet("/setting", (IConfiguration configuration, string key) =>
 {
     return configuration[key];
@@ -76,20 +60,8 @@ app.MapGet("/setting", (IConfiguration configuration, string key) =>
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapGet("/authenticated-ping", () =>
-{
-    return "pong";
-})
-.RequireAuthorization()
-;
-
 app.MapTodoEndpoints();
 
 await app.RunAsync();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
 
 interface IApiMarker;
