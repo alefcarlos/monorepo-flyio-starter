f10a32506c56b0c50c5b7755839e548293fe034f
alefcarlos <alef.carlos@gmail.com>
2026-05-29 20:30:57 -0300

    fix: refactor to use record

--- src/Flyio.Demo.Todos.Contracts/TodoIsDoneIntegrationEvent.cs
+++ src/Flyio.Demo.Todos.Contracts/TodoIsDoneIntegrationEvent.cs
@@ -2,12 +2,4 @@ using Mediator;
 
 namespace Flyio.Demo.Todos.Contracts;
 
-public class TodoIsDoneIntegrationEvent : INotification
-{
-    public TodoIsDoneIntegrationEvent(TodoId id)
-    {
-        Id = id;
-    }
-
-    public TodoId Id { get; }
-}
+public record TodoIsDoneIntegrationEvent(TodoId Id) : INotification;
